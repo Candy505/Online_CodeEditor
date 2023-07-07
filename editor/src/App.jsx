a80f9fdf6f6f5a5032dc875450ceda2b683b3116
Candy505 <amayanegi42@gmail.com>
2023-07-07 21:08:13 +0530

    structure defined

--- editor/src/App.jsx
+++ editor/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Landing from './components/Landing'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Landing/>
+     <div className='App'>
+      <div className='ControlBox'>     </div>
+      <div className='PanelBox'>  </div>
+     </div>
     </>
   )
 }
