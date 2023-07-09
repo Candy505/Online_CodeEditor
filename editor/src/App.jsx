185d0cf44f25a173e7d43bf8b0b657cd17133cba
Candy505 <amayanegi42@gmail.com>
2023-07-10 00:15:38 +0530

    indentation function added

--- editor/src/App.jsx
+++ editor/src/App.jsx
@@ -5,6 +5,7 @@ import { Highlighter } from "./components/Highlighter";
 import * as themes from "react-syntax-highlighter/dist/esm/styles/hljs";
 import * as languages from "react-syntax-highlighter/dist/esm/languages/hljs";
 
+
 import './App.css';
 
 const defaultLanguage = "javascript" || Object.keys(languages).sort()[0];
@@ -12,18 +13,27 @@ const defaultTheme = "atomOneDark" || Object.keys(themes).sort()[0];
 
 function App() {
 
+
+  
+
   const [input, setInput] = useState("");
   const[theme,setTheme]  = useState(defaultTheme);
   const[language,setLanguage] = useState(defaultLanguage);
-
+ 
+  function handleChange(e)
+  {
+    setLanguage(e.target.value);
+    setInput('');
+  }
 
   return (
   
      <div className='App'>
-      <div className='ControlsBox'> 
+
+<div className='ControlsBox'> 
       <Dropdown
           defaultLanguage={defaultLanguage}
-          onChange={(e) => setLanguage(e.target.value)}
+          onChange={(e) => handleChange(e)}
           data={languages}
         />
         
@@ -43,6 +53,7 @@ function App() {
           {input}
         </Highlighter>
       </div>
+     
      </div>
  
   );
--- editor/src/components/Highlighter.jsx
+++ editor/src/components/Highlighter.jsx
@@ -2,13 +2,44 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import "./Highlighter.css";
 
 export const Highlighter = ({ language, theme, children }) => {
+  const indentedCode = indentCode(children);
+
   return (
     <SyntaxHighlighter
       language={language}
       style={theme}
       className="highlighter"
     >
-      {children}
+      {indentedCode}
     </SyntaxHighlighter>
   );
 };
+
+
+function indentCode(code) {
+ 
+  const lines = code.split('\n');
+  let indentedCode = '';
+
+  let indentLevel = 0;
+  const indentSize = 2; // Number of spaces per indentation level
+
+  for (let line of lines) {
+    line = line.trim(); // Remove leading and trailing whitespace
+
+    if (line.endsWith('}')) {
+      indentLevel--;
+    }
+
+    indentedCode += ' '.repeat(indentLevel * indentSize) + line + '\n';
+
+    if (line.endsWith('{')) {
+      indentLevel++;
+    }
+  }
+
+  return indentedCode.trim(); 
+  // Add indentation logic for other languages if needed
+
+  return code;
+}
